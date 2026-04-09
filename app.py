e3384293b82bfec9f8073a1352db33d83049e9c4
Angie0945 <ajvargasp@eafit.edu.co>
2026-04-09 13:49:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ import tempfile
 import platform
 
 # LangChain
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter as CharacterTextSplitter
 from langchain_community.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_community.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 
